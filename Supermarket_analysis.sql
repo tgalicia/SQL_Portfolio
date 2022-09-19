3133900ca9cde34ab2f0775cb9fa42981e06e15e
Tiffany Galicia <63023544+tgalicia@users.noreply.github.com>
2022-09-18 21:07:23 -0500

    Update Supermarket_analysis.sql

--- Supermarket_analysis.sql
+++ Supermarket_analysis.sql
@@ -2,9 +2,10 @@
 Analysis for Gross Sales of Product line by gender
 */
 
-select gender, product_line, sum(gross_total)
+select gender, product_line, sum(gross_total) as product_gross_total
 from supermarket_sales
-order by product_line, gender;
+group by product_line, gender
+order by product_gross_total;
 
 /*
 Analysis for Gross Sales by store
@@ -12,7 +13,7 @@ Analysis for Gross Sales by store
 
 select branch, sum(gross_total)
 from supermarket_sales
-order by branch;
+group by branch;
 
 /*
 Analysis for Product line by store
@@ -21,5 +22,5 @@ Analysis for Product line by store
 
 select branch, product_line, sum(gross_total)
 from supermarket_sales
-order by branch, product_line;
+group by branch, product_line;
 
