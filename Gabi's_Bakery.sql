60cf5ed333646ac4777698638056b1ebb10010cb
Tiffany Galicia <63023544+tgalicia@users.noreply.github.com>
2022-09-19 21:34:06 -0500

    Update Gabi's_Bakery.sql

--- Gabi's_Bakery.sql
+++ Gabi's_Bakery.sql
@@ -6,24 +6,25 @@ CREATE TABLE gabi_bakery_inventory(
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     quantity INT NOT NULL
+    cost FLOAT NOT NULL
 ); INSERT INTO gabi_bakery_inventory
-(name, quantity) VALUES("chocolate chip cookie", 3);
+(name, quantity, cost) VALUES("chocolate chip cookie", 3, 4.99);
 INSERT INTO gabi_bakery_inventory
-(name, quantity) VALUES("oatmeal raisin cookie", 4);
+(name, quantity, cost) VALUES("oatmeal raisin cookie", 4, 4.99);
 INSERT INTO gabi_bakery_inventory
- (name, quantity)VALUES("sugar cookie", 1);
+(name, quantity, cost) VALUES("sugar cookie", 1, 4.99);
 INSERT INTO gabi_bakery_inventory
-(name, quantity) VALUES("chocolate pie slice", 8);
+(name, quantity, cost) VALUES("chocolate pie slice", 8, 6.50);
 INSERT INTO gabi_bakery_inventory
-(name, quantity) VALUES("brownie", 1);
+(name, quantity, cost) VALUES("brownie", 1, 3.25);
 INSERT INTO gabi_bakery_inventory
-(name, quantity) VALUES("vanilla cupcake", 3);
+(name, quantity, cost) VALUES("vanilla cupcake", 3, 5);
 INSERT INTO gabi_bakery_inventory
-(name, quantity) VALUES("chocolate cupcake", 7);
+(name, quantity, cost) VALUES("chocolate cupcake", 7, 5);
 INSERT INTO gabi_bakery_inventory
-(name, quantity) VALUES("double chocolate cookie", 6);
+(name, quantity, cost) VALUES("double chocolate cookie", 6, 5.50);
 INSERT INTO gabi_bakery_inventory
-(name, quantity) VALUES("white chocolate cookie", 13);
+(name, quantity, cost) VALUES("white chocolate cookie", 13, 5.50);
 
 
 /*
